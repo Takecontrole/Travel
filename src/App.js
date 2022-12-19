ebd501c160fdefc9fc972867f74f1f1f5720e43a
Takecontrole <takecontrole2034@gmail.com>
2022-12-19 19:12:57 +0400

    rr

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
 import Home from "./pages/home/Home";
-import Hotel from "./pages/hotel/Hotel";
-import List from "./pages/list/List";
+
 
 function App() {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/hotels" element={<List/>}/>
-        <Route path="/hotel" element={<List/>}/>
-        <Route path="/hotels/:id" element={<Hotel/>}/>
-      </Routes>
-    </HashRouter>
+    <Home/>
   );
 }
 
