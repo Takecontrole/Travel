37b31bb7909d4316f38495e92d486f313932ba16
Takecontrole <113514590+Takecontrole@users.noreply.github.com>
2022-12-20 12:29:36 +0400

    Add link

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,14 +1,17 @@
 import "./featured.css";
+import {Link} from "react-router-dom";
 
 const Featured = () => {
   return (
     <div className="featured">
       <div className="featuredItem">
+       <Link to="/hotels">
         <img
           src="https://images.unsplash.com/photo-1500916434205-0c77489c6cf7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmV3JTIweW9ya3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60"
           alt=""
           className="featuredImg"
         />
+       </Link>
         <div className="featuredTitles">
           <h1>New-York</h1>
           <h2>163 отелей</h2>
